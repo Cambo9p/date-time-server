56c703a9693f5198205a20110aa6e5dd71f39678
Cam <cambo9p.11@gmail.comgit config  user.email cambo9p.11@gmail.com>
2022-08-23 23:26:48 +1200

    client will now recieve bytearray from server

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import socket
 import sys
+import select
 
 BUFSIZE = 6  # size of the packet that will be recieved from server
 
@@ -7,17 +8,38 @@ BUFSIZE = 6  # size of the packet that will be recieved from server
 class Date_time_client(object):
     """the class for the date time client"""
 
-    def __init__(self, port=6969, host=socket.gethostbyname(
-                                    socket.gethostname()), req_type='date'):
-        self.request_type = req_type
-        self.ADDR = (host, port)
+    def __init__(self, port, host_ip, req_type='date'):
+        self.request_type = str(req_type)
+        self.port = port
+        self.ip_addr = host_ip
+        self.ADDR = (host_ip, int(port))
         self.client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.client.connect(self.ADDR)
+
+    def recieve_response(self):
+        """the client will wait to recieve a responce from the server"""
+
+        inputs = [self.client]
+        self.outputs = []
+
+        try:
+            inputready, outputready, exceptready = select.select(inputs,
+                                                                 self.outputs,
+                                                                 [], 10)
+            message = inputready[0].recv(1024)
+            print("message recieved: ", message)
+
+        except IndexError:
+            print("ERROR: more than one second to recieve response")
 
     def send_request(self):
         """sends a request to the server"""
         request_packet = self.create_request_packet()
-        self.client.send(request_packet)
+        # self.client.send(request_packet)
+        print('REQUEST PACKET ', request_packet, 'ADDR ', (str(self.ip_addr),
+                                                           int(self.port)))
+        self.client.sendto(request_packet, (str(self.ip_addr), int(self.port)))
+        # self.client.close()
 
     def create_request_packet(self) -> bytearray:
         """Creates the dt-request packet to send to the server"""
@@ -52,7 +74,7 @@ def return_args():
 
     # check that the values are correct
     check_request(request_type)
-    ip_addr = check_ip_addr()
+    ip_addr = check_ip_addr(ipaddress)
     check_port(port_number)
 
     return request_type, ip_addr, port_number
@@ -71,14 +93,14 @@ def check_ip_addr(ip_addr) -> str:
 
 def check_port(port) -> None:
     """checks to see if the port number is correct"""
-    if port < 1024 or port > 64000:
+    if int(port) < 1024 or int(port) > 64000:
         print("ERROR: invalid port number")
         quit()
 
 
 def check_request(request_type) -> None:
     """checks that the request type is valid"""
-    if request_type != "date" or request_type != "time":
+    if str(request_type) != "date" and request_type != "time":
         print("ERROR: invalid request type '{}'".format(request_type))
         quit()
 
@@ -86,9 +108,10 @@ def check_request(request_type) -> None:
 def main():
     """the main function for the client"""
 
-    # req_type = get_request_type()
-    client = Date_time_client()
+    request_type, ip_addr, port_number = return_args()
+    client = Date_time_client(port_number, ip_addr, request_type)
     client.send_request()
+    client.recieve_response()
 
 
 main()
